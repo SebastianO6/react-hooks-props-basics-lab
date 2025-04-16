0cc49518ac3cf15762d6411fef306b878a1b1fa9
Bill <bill.omondi@student.moringaschool.com>
2025-04-16 06:37:03 +0300

    basic props

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,31 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+function Bio({ bio }) {
+  if (!bio) {
+    return null;
+  } else {
+    return (
+      <div>
+        <p>{bio}</p>
+      </div>
+    );
+  }
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+
+      {/* Use the Bio component here */}
+      <Bio bio={props.bio} />
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+
+      {/* Use the Links component */}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
